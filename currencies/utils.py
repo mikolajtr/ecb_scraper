6f9d9ff691a98954b5a1c86a7c8d53861adc2bae
Mikolaj Trawinski <kontakt@mikolajtrawinski.pl>
2018-10-19 01:18:43 +0200

    added HTML parser, RSS feeds parser and requests library, implemented ECB page scraper and ECB currency RSS reader

---
+++ currencies/utils.py
@@ -0,0 +1,34 @@
+import re
+from decimal import Decimal
+
+import feedparser
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_feeds_urls(ecb_url, base_url):
+    page = requests.get(ecb_url)
+    html = BeautifulSoup(page.text, features="html.parser")
+    for a in html.find_all('a', href=re.compile("/rss/fxref")):
+        url = base_url + a['href']
+        currency = a['href'][11:14].upper()
+        yield {
+            "url": url,
+            "currency": currency
+        }
+
+
+def get_currency_changes(url):
+    feed = feedparser.parse(url)
+    entries = feed.entries
+
+    for entry in entries:
+        title_split = entry.title.split()
+        updated = entry.updated_parsed
+        value, currency = title_split[0], title_split[1]
+
+        yield {
+            "currency": currency,
+            "value": Decimal(value),
+            "updated": updated
+        }
