8b1b85bf6165d73430eae5f454060914b38cdc03
Mikolaj Trawinski <kontakt@mikolajtrawinski.pl>
2018-10-19 02:36:06 +0200

    results pagination

--- currencies/views.py
+++ currencies/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.views import View
 
@@ -6,7 +7,15 @@ from currencies.models import ExchangeRate
 
 class ExchangeRatesView(View):
     def get(self, request):
-        exchanges_rates = ExchangeRate.objects.all()
+        page = int(request.GET.get('page', 1))
+        page_size = int(request.GET.get('page_size', 20))
+
+        exchanges_rates = ExchangeRate.objects.all().order_by('-updated')
+        paginator = Paginator(exchanges_rates, page_size)
+
         return JsonResponse({
-            "exchange_rates": list(exchanges_rates)
+            "page": page,
+            "page_size": page_size,
+            "total": paginator.count,
+            "data": list(paginator.page(page))
         })
