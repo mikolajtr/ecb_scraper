2b32ee872ea86d6de5ab4484a8a44cea9d197f38
Mikolaj Trawinski <kontakt@mikolajtrawinski.pl>
2018-10-19 02:57:26 +0200

    added command to scrap rates to database

---
+++ currencies/management/commands/updateexchangerates.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from time import mktime
+
+from django.core.management import BaseCommand
+
+from currencies.models import ExchangeRate
+from currencies.utils import get_feeds_urls, get_exchange_rates
+
+
+class Command(BaseCommand):
+    help = "Get the first entry about exchange rate for every currency in feeds"
+
+    def handle(self, *args, **options):
+        feeds_urls = get_feeds_urls("https://www.ecb.europa.eu/home/html/rss.en.html", "https://www.ecb.europa.eu")
+
+        for url in feeds_urls:
+            exchange_rates = get_exchange_rates(url)
+            for rate in exchange_rates:
+                exchange_rate = ExchangeRate(currency=rate['currency'],
+                                             value=rate['value'],
+                                             updated=datetime.fromtimestamp(mktime(rate['updated'])))
+                print('save')
+                exchange_rate.save()
+                break
